0d14d67477a59054b4eaa3578e38e3b4c9791398
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2018-04-27 21:29:20 +0530

    Trying heroku

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+let google = {
+  clientId:
+    "611812101769-tqrtsfikl5d3de24i1ocl78hac5caoou.apps.googleusercontent.com",
+  clientSecret: "0dOMkb1v261dkb1VBMtkRF5R",
+  user: "ankitbrahmbhatt1997@gmail.com",
+  reciever: "ukogdna6560@gmail.com",
+  refreshToken: "1/VSir9UwnuQlXCRG7BK2NR1EVrm8kJ-otp2EdmjXeMFc",
+  accessToken:
+    "ya29.GlunBTucjP1F9I6vtGZm3H76RtuMeBX4krpkFIOw8XumMVmKRIMjAijawT-24pIVGKu00d8MwU0yXF0VBuMjCnFZI8vIuCTTcmCl6zcfQTxHbbRV5s_ZXJBmjvJG"
+};
+
+module.exports = google;
